e3725a785acfc3ea36926c41898d2e33ee97252d
teoro <teoro@users.noreplay.github.com>
2024-03-22 13:31:43 +0200

    Proiect POO!

--- main.cpp
+++ main.cpp
@@ -1,48 +1,296 @@
 #include <iostream>
-#include <array>
+#include <cstdlib>
+#include <string>
+#include <ctime>
+#include <utility>
+#include <vector>
 
-int main() {
-    std::cout << "Hello, world!\n";
-    std::array<int, 100> v{};
-    int nr;
-    std::cout << "Introduceți nr: ";
-    /////////////////////////////////////////////////////////////////////////
-    /// Observație: dacă aveți nevoie să citiți date de intrare de la tastatură,
-    /// dați exemple de date de intrare folosind fișierul tastatura.txt
-    /// Trebuie să aveți în fișierul tastatura.txt suficiente date de intrare
-    /// (în formatul impus de voi) astfel încât execuția programului să se încheie.
-    /// De asemenea, trebuie să adăugați în acest fișier date de intrare
-    /// pentru cât mai multe ramuri de execuție.
-    /// Dorim să facem acest lucru pentru a automatiza testarea codului, fără să
-    /// mai pierdem timp de fiecare dată să introducem de la zero aceleași date de intrare.
-    ///
-    /// Pe GitHub Actions (bife), fișierul tastatura.txt este folosit
-    /// pentru a simula date introduse de la tastatură.
-    /// Bifele verifică dacă programul are erori de compilare, erori de memorie și memory leaks.
-    ///
-    /// Dacă nu puneți în tastatura.txt suficiente date de intrare, îmi rezerv dreptul să vă
-    /// testez codul cu ce date de intrare am chef și să nu pun notă dacă găsesc vreun bug.
-    /// Impun această cerință ca să învățați să faceți un demo și să arătați părțile din
-    /// program care merg (și să le evitați pe cele care nu merg).
-    ///
-    /////////////////////////////////////////////////////////////////////////
-    std::cin >> nr;
-    /////////////////////////////////////////////////////////////////////////
-    for(int i = 0; i < nr; ++i) {
-        std::cout << "v[" << i << "] = ";
-        std::cin >> v[i];
+class Player{
+private:
+    int hp = 500, exp = 0, level = 0, gold = 100, atk = 20, lhp = hp;
+    std::string name;
+public:
+    Player(std::string name, int hp, int atk, int gold, int exp, int level)
+    {
+        this->name = std::move(name);
+        this->hp = hp;
+        this->atk = atk;
+        this->gold = gold;
+        this->exp = exp;
+        this->level = level;
     }
-    std::cout << "\n\n";
-    std::cout << "Am citit de la tastatură " << nr << " elemente:\n";
-    for(int i = 0; i < nr; ++i) {
-        std::cout << "- " << v[i] << "\n";
+
+    friend std::ostream& operator<<(std::ostream& out, const Player& player);
+
+//    std::string getName()const{return name;}
+//    int getHp()const{return hp;}
+    [[nodiscard]] int getAtk()const{return atk;}
+//    int getGold()const{return gold;}
+//    int getExp()const{return exp;}
+//    int getLevel()const{return level;}
+//    void setName(std::string name){this->name = name;}
+//    void setHp(int hp){this->hp = hp;}
+//    void setAtk(int atk){this->atk = atk;}
+//    void setGold(int gold){this->gold = gold;}
+//    void setExp(int exp){this->exp = exp;}
+//    void setLevel(int level){this->level = level;}
+
+    void takeDmg(int damage)
+    {
+        hp -= damage;
+        if(hp < 0) hp = 0;
+        std::cout << name << " a suferit " << damage << " damage. " << name << " are acum " << hp << " health." << std::endl;
     }
-    ///////////////////////////////////////////////////////////////////////////
-    /// Pentru date citite din fișier, NU folosiți tastatura.txt. Creați-vă voi
-    /// alt fișier propriu cu ce alt nume doriți.
-    /// Exemplu:
-    /// std::ifstream fis("date.txt");
-    /// for(int i = 0; i < nr2; ++i)
-    ///     fis >> v2[i];
+
+    [[nodiscard]] bool isAlive() const
+    {
+        return hp > 0;
+    }
+
+    void player_earnExp(int experience)
+    {
+        exp += experience;
+    }
+
+    void earnGold(int coins)
+    {
+        gold += coins;
+    }
+
+    void LevelUp_Player()
+    {
+        const int expPerLvl = 1000;
+        if (exp >= expPerLvl)
+        {
+            level++;
+            exp -= expPerLvl;
+            hp = lhp + 40;
+            lhp = hp;
+            atk += 10;
+            std::cout << "Felicitari! Ai acum nivelul: " << level << "!" << '\n';
+        }
+    }
+    ~Player()=default;
+};
+
+std::ostream& operator<<(std::ostream& out, const Player& player) {
+    out << "Name: " << player.name << '\n';
+    out << "HP: " << player.hp << "; DMG: " << player.atk << "; GOLD: " << player.gold << "; EXP: " << player.exp<< "; LEVEL: " << player.level << '\n';
+    return out;
+}
+
+class Mob{
+private:
+    int hp, exp, gold, atk, level, ihp;
+    std::string name;
+public:
+    Mob(std::string name, int hp, int atk, int gold, int exp, int level)
+    {
+        this->name = std::move(name);
+        this->hp = hp;
+        this->ihp = hp;
+        this->atk = atk;
+        this->gold = gold;
+        this->exp = exp;
+        this->level = level;
+    }
+
+    friend std::ostream& operator<<(std::ostream& out, const Mob& mob);
+
+    [[nodiscard]] std::string getName()const{return name;}
+//    int getHp()const{return hp;}
+    [[nodiscard]] int getAtk()const{return atk;}
+    [[nodiscard]] int getGold()const{return gold;}
+    [[nodiscard]] int getExp()const{return exp;}
+//    int getLevel()const{return level;}
+//    void setName(std::string name){this->name = name;}
+//    void setHp(int hp){this->hp = hp;}
+//    void setAtk(int atk){this->atk = atk;}
+//    void setGold(int gold){this->gold = gold;}
+//    void setExp(int exp){this->exp = exp;}
+//    void setLevel(int level){this->level = level;}
+
+    void takeDmg(int damage)
+    {
+        hp -= damage;
+        if(hp < 0) hp = 0;
+        std::cout << name << " a suferit " << damage << " damage. " << name << " are acum " << hp << " health." << std::endl;
+    }
+
+    [[nodiscard]] bool isAlive() const
+    {
+        return hp > 0;
+    }
+
+    void reset_mob_hp()
+    {
+        hp = ihp;
+    }
+
+    void LevelUp_Mob()
+    {
+        level++;
+        hp = ihp + 25;
+        ihp = hp;
+        atk += 10;
+    }
+    ~Mob()=default;
+};
+
+std::ostream& operator<<(std::ostream& out, const Mob& mob) {
+    out << "Name: " << mob.name << '\n';
+    out << "HP: " << mob.hp << "; DMG: " << mob.atk << '\n';
+    return out;
+}
+
+class Attack {
+public:
+    static void performAttack(Player& player, Mob& mob)
+    {
+        mob.takeDmg(player.getAtk());
+        if (mob.isAlive())
+        {
+            player.takeDmg(mob.getAtk());
+        }
+    }
+    ~Attack()=default;
+};
+
+//class Shop{
+//private:
+//    int hp_pot_small, hp_pot_medium, hp_pot_big;
+//public:
+//
+//};
+
+int main()
+{
+    srand(time(nullptr));
+    std::cout<<"    Bine te-am gasit!"<<'\n';
+    std::cout<<"    Incepem printr-un mic tutorial, asa de inceput. In mare parte tu vei apasa doar pe niste butoane,"<<'\n';
+    std::cout<<"nimic mai mult! Dar trebuie sa ai mare grija pe ce apesi ca vei putea muri si pierde tot progresul :("<<'\n';
+    std::cout<<"Joculetul este asemanator cu Greedy Cave. Ai un jucator, mobi cu care sa te lupti si poti obtine dropuri."<<'\n';
+    std::cout<<"Dropurile te vor ajuta pe parcursul drumului deoarece iti vor imbunatatii stats-urile si astfel sa iesi"<<'\n';
+    std::cout<<"victorios din grota!"<<'\n';
+    std::cout<<"    P.S: Ai grija sa nu opresti rularea programului ca te ai dus...pierzi tot progresul din pacate ://"<<'\n';
+    std::cout<<'\n'<<'\n';
+    std::string name;
+    std::cout<<"Alege numele tau: ";
+    std::cin>>name;
+    Player player(name, 500, 20, 100, 0, 0);
+    std::cout<<'\n'<<'\n'<<"Salut "<< name <<'\n';
+    std::cout<<'\n'<<'\n';
+    std::cout<<"Acum cu interactiunile:"<<'\n';
+    std::cout<<"1.Start"<<'\n';
+    std::cout<<"2.Exit"<<'\n';
+    std::vector<Mob> enemies = {
+            Mob("Cave Zombie", 100, 10, 45, 175, 0),
+            Mob("McWolf", 60, 15, 25, 150, 0),
+            Mob("Gorlock the Destroyer", 250, 25, 75, 250, 0)
+    };
+    int interact1;
+    std::cin>>interact1;
+    if(interact1 == 1)
+    {
+        std::cout<<'\n'<<'\n'<<'\n'<<'\n'<<'\n'<<'\n'<<'\n'<<'\n';
+        std::cout<<"    Esti un calator doritor de aventura. Ai citit cartea pe care bunicul tau ti-a lasat-o inainte ca acesta sa moara"<<'\n';
+        std::cout<<"si afli de un oras numit Evanghelion cu o istorie spectaculoasa si care contine o pestera intunecata plina de monstri"<<'\n';
+        std::cout<<"infricosatori care cel mai probabil doresc sa-ti manance creierul...hehe. Desigur, daca reusesti sa-i omori vei capata"<<'\n';
+        std::cout<<"iteme de valoare sau care te vor ajuta in noi aventuri periculoase."<<'\n';
+        std::cout<<"    Te decizi sa pornesti in aceasta calatorie de unul singur spre asa numitul 'Solo Leveling Cave'."<<'\n';
+        std::cout<<"    Dupa o lunga calatorie, de o zi si o noapte, ajungi in Evanghelion, iar niste localnici de treaba iti arata drumul"<<'\n';
+        std::cout<<"spre pestera intunecata."<<'\n';
+        std::cout<<"    Iti iei inima-n dinti si faci primul pas in pestera. Aprinzi o torta si continui sa avansezi."<<'\n';
+        std::cout<<"    'Am vrut pestera eu...da...cu putoarea asta mai bine-mi iau direct viata si termin mai repede...'"<<'\n';
+        std::cout<<"    Lasand la o parte dorintele de suicid, continui sa inaintezi si auzi in departare un raget puternic.";
+        std::cout<<'\n'<<'\n'<<"................"<<'\n'<<'\n';
+        std::cout<<"    Este primul monstru cu care te intalnesti? Daca da, ai patru posibilitati in momentul de fata. Ce alegi?";
+        std::cout<<'\n'<<'\n'<<"................"<<'\n'<<'\n';
+        int interact2 = 1;
+        int v[3];
+        v[0]=v[1]=v[2]=0;
+        while(interact2 != 0)
+        {
+            //int mobIndex = rand() % enemies.size();
+            int mobIndex = rand() % 3;
+            Mob& currentEnemy = enemies[mobIndex];
+            std::cout<<"Ai in fata ta un "<<currentEnemy.getName()<<'\n'<<'\n';
+            std::cout << "1.Ataca" << '\n';
+            std::cout << "2.Mob Stats" << '\n';
+            std::cout << "3.Fugi" << '\n';
+            std::cout << "0.Exit Cave" << '\n';
+            std::cin >> interact2;
+            switch (interact2) {
+                case 1:
+                    while (player.isAlive() && currentEnemy.isAlive()) {
+                        Attack::performAttack(player, currentEnemy);
+                    }
+                    v[mobIndex]++;
+                    if (player.isAlive()) {
+                        std::cout << '\n' << '\n';
+                        std::cout << name << " wins!" << std::endl;
+                        std::cout << '\n' << '\n';
+                        currentEnemy.reset_mob_hp();
+                        if(v[mobIndex] % 4 == 0)
+                            currentEnemy.LevelUp_Mob();
+                        player.player_earnExp(currentEnemy.getExp());
+                        player.earnGold(currentEnemy.getGold());
+                        player.LevelUp_Player();
+                        std::cout << "Informatii despre jucator:\n";
+                        std::cout << player << '\n' << '\n';
+                    }
+                    else {
+                        std::cout << currentEnemy.getName() << " wins!" << std::endl;
+                        std::cout << '\n' << '\n';
+                        std::cout << "--------GAME OVER--------";
+                        std::cout << '\n' << '\n';
+                        interact2 = 0;
+                    }
+
+                    break;
+                case 2:
+                    std::cout << '\n' << '\n';
+                    std::cout << "Informatii despre monstru:\n";
+                    std::cout << enemies[mobIndex] << '\n' << '\n';
+                    std::cout << "1.Ataca" << '\n';
+                    std::cout << "2.Fugi" << '\n';
+                    std::cout << "0.Exit Cave" << '\n';
+                    std::cin >> interact2;
+                    switch (interact2) {
+                        case 1:
+                            while (player.isAlive() && currentEnemy.isAlive()) {
+                                Attack::performAttack(player, currentEnemy);
+                            }
+                            v[mobIndex]++;
+                            if (player.isAlive()) {
+                                std::cout << '\n' << '\n';
+                                std::cout << name << " wins!" << std::endl;
+                                std::cout << '\n' << '\n';
+                                currentEnemy.reset_mob_hp();
+                                if(v[mobIndex] % 4 == 0)
+                                    currentEnemy.LevelUp_Mob();
+                                player.player_earnExp(currentEnemy.getExp());
+                                player.earnGold(currentEnemy.getGold());
+                                player.LevelUp_Player();
+                                std::cout << "Informatii despre jucator:\n";
+                                std::cout << player << '\n' << '\n';
+                            }
+                            else {
+                                std::cout << currentEnemy.getName() << " wins!" << std::endl;
+                                std::cout << '\n' << '\n';
+                                std::cout << "--------GAME OVER--------";
+                                std::cout << '\n' << '\n';
+                                interact2 = 0;
+                            }
+                            break;
+                        case 2:
+                            break;
+                    }
+                    break;
+                case 3:
+                    break;
+            }
+        }
+    }
     return 0;
 }
